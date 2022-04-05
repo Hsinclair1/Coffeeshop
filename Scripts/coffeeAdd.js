999624f4b191bc87dc869ab39dda447339ac31c6
Hsinclair1 <sin21004503@btc.ac.uk>
2022-04-05 14:41:10 +0100

    coffee4

--- Scripts/coffeeAdd.js
+++ Scripts/coffeeAdd.js
@@ -205,7 +205,6 @@ function postIngredient(ingredient) {
     $.ajax({
         url: "https://api-coffeeservice.herokuapp.com/api/ingredients",
         type: "post",
-        contentType: "application/json",
         headers: { "ApiKey": ApiKey },
         data: { name: ingredient.name, uom: ingredient.uom },
         success: function(result) {
